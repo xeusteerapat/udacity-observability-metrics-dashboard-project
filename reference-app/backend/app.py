1165039eb230aa3c30f699b4bc271e6155be9da5
xeusteerapat <xeus085@gmail.com>
2022-06-16 21:59:55 +0700

    :sparkles: [Update] update tracing backend

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -54,26 +54,54 @@ tracer = init_tracer("backend-service")
 
 
 @app.route("/")
+@by_full_path_counter
+@by_endpoint_counter
 def homepage():
-    return "Hello World"
+    app.logger.info('Hit the homepage')
+    with tracer.start_span('homepage-span') as span:
+        span.set_tag('homepage-tag', '95')
+        return "Hello World"
 
 
 @app.route("/api")
+@by_full_path_counter
+@by_endpoint_counter
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    app.logger.info('Hit the /api endpoint')
+    with tracer.start_span('my_api_span') as span:
+        span.set_tag('my_api-tag', '90')
+        answer = "something"
+        return jsonify(reponse=answer)
 
 
-@app.route("/star", methods=["POST"])
+@app.route('/star', methods=['POST'])
+@by_full_path_counter
+@by_endpoint_counter
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
-    return jsonify({"result": output})
+    app.logger.info('Hit the /star endpoint')
+
+    with tracer.start_span('star_span') as span:
+        span.set_tag('star-tag', '80')
+
+        try:
+            star = mongo.db.stars
+            name = request.json['name']
+            distance = request.json['distance']
+            star_id = star.insert({'name': name, 'distance': distance})
+            new_star = star.find_one({'_id': star_id})
+            output = {'name': new_star['name'],
+                      'distance': new_star['distance']}
+            return jsonify({'result': output})
+        except Exception as e:
+            print(e)
+
+
+@app.route('/error')
+@by_full_path_counter
+@by_endpoint_counter
+def oops():
+    return ':(', 500
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
