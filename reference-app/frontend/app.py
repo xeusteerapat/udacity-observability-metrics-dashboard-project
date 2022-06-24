9a41565b3d47dffff95daee837439ec8f6632ca4
xeusteerapat <xeus085@gmail.com>
2022-06-24 22:47:46 +0700

    :goal_net: [Update] catch errors

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -46,5 +46,60 @@ def homepage():
         return render_template("main.html")
 
 
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route("/403")
+def status_code_403():
+    status_code = 403
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/404")
+def status_code_404():
+    status_code = 404
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/500")
+def status_code_500():
+    status_code = 500
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
+@app.route("/503")
+def status_code_503():
+    status_code = 503
+    raise InvalidUsage(
+        "Raising status code: {}".format(status_code), status_code=status_code
+    )
+
+
 if __name__ == "__main__":
     app.run(threaded=True)
